0bfddf133f1ee292b7948be9f29a1c376f64066f
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-16 12:11:15 -0300

    feat: change timer

--- assets/js/spotsLeft.js
+++ assets/js/spotsLeft.js
@@ -1,16 +1,16 @@
 const vagas = document.getElementById('vagas-restantes');
 let timer = 11000;
 
-var vagasRestantes = 1000;
+var vagasRestantes = 18;
 
 setTimeout(() => {
   var downloadTimer = setInterval(function(){
     if(vagasRestantes <= 3){
       clearInterval(downloadTimer);
-      document.getElementById("vagas-restantes").innerHTML = vagasRestantes + " vagas disponíveis de 1000.";
+      document.getElementById("vagas-restantes").innerHTML = '<h3 class="text-danger timer">ATENÇÃO</h3><p><span class="text-danger">' + vagasRestantes +'</span> vagas disponíveis de 1000.</span>';
     } else {
-      document.getElementById("vagas-restantes").innerHTML = vagasRestantes + " vagas disponíveis de 1000.";
+      document.getElementById("vagas-restantes").innerHTML = '<h3 class="text-danger timer">ATENÇÃO</h3><p><span class="text-danger">' + vagasRestantes +'</span> vagas disponíveis de 1000.</span>';
     }
     vagasRestantes -= 1;
   }, timer);
-}, 1105000);
+}, 2000);
